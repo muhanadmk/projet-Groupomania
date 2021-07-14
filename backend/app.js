2b6793749b4b6cbc6b3f3ace6d245540e74731b5
muhanad <mouhandmk1@gmail.com>
2021-07-14 11:01:59 +0200

    cree un user mysql et sécurisé le détail de connexion

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const cors = require('cors');
+
 // const limiter = require('./middleware/limit');
 // const stuffRoutes = require('./routes/stuff');
 // const userRoutes = require('./routes/user');
 
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv')
+dotenv.config();
 // ajouter le helmet pour protéger http header
 const helmet = require('helmet');
 
 // Create connection à mysql
 const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'groupomania'
+  host     : process.env.HOST,
+  user     : process.env.USERNAME,
+  password : process.env.PASSWORD,
+  database : process.env.DATABASE,
+  port     : process.env.DB_PORT
 });
 
 // Connect
@@ -38,7 +42,21 @@ app.get('/createpoststable', (req, res) => {
   });
 });
 
+app.post('/adduser', (req, res) => {
+  let post = {email:'mouhandada@gmai.com', password:'156488489'};
+  let sql = 'INSERT INTO user SET ?';
+  let query = db.query(sql, post, (err, result) => {
+      if(err) throw err;
+      console.log(result);
+      res.send('Post 2 added...');
+  });
+});
+
+app.get('/getall', (req, res)=>{
+console.log('muhanad');
+});
 
 app.use(express.json());
+app.use(cors());
 
 module.exports = app;
