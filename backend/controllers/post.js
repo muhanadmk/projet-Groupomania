bada9990452de3b393868076eeac8f85540c81b3
muhanadmk <mouandmk1@gmail.com>
2021-07-26 13:48:28 +0200

    deleted post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -18,3 +18,50 @@ exports.writePost = (req, res) => {
     res.status(201).json({ message: 'post créé !' });
   });
 }
+
+exports.deletePost = (req, res, next) => {
+  const idPost = req.params.id;
+  db.query('SELECT `user_id` FROM `posts` WHERE id = ?', idPost, function (err, result) {
+    if (err) throw err;
+    const userid = result[0].user_id;
+    userId = req.body.userId;
+    // console.log(userid);
+    // console.log(userId);
+    if (userid == userId) {
+      // console.log('good');
+      db.query('DELETE FROM `posts` WHERE id = ?', idPost, function (err, result) {
+        if (err) {
+          throw err;
+        }
+         res.status(204).json({ message: 'post deleted !' });
+      });
+    }
+    else {
+      // console.log('baaaaaad');
+       res.status(401).json({ message: 'Invalid user ID !' });
+    };
+  });
+}
+
+exports.modifierPost = (req, res) => {
+  const idPost = 13;
+  console.log(idPost);
+  db.query('SELECT `user_id` FROM `posts` WHERE id = ?', idPost, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    const userid = result[0].user_id;
+    userId = req.body.userId;
+    console.log(userid);
+    console.log(userId);
+    if (userId === userid) {
+      db.query(`UPDATE posts SET post='0000000000' WHERE id = ?`, idPost, (err, result) => {
+        if (err) throw err;
+        console.log(result[0]);
+        res.status(200).json({ message: 'post modifié !' });
+      })
+    }
+  });
+}
+
+
--- backend/routers/post.js
+++ backend/routers/post.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const postCtlr = require('../controllers/post');
 
-router.post('/sendpost', postCtlr.writePost);
+router.post('/writePost', postCtlr.writePost);
+router.delete('/deletePost/:id', postCtlr.deletePost);
+router.put('/modifierPost', postCtlr.modifierPost);
+
+
 
 
 module.exports = router
